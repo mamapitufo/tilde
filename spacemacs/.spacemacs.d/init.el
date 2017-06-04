3e9a7d49e9cf6f53c929eee97fa1a66b16929499
Alberto Brealey-Guzman <b@feltfragments.com>
2017-06-04 15:39:20 +0100

    Use `jk` as ESC in spacemacs

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -309,6 +309,7 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+  (setq-default evil-escape-key-sequence "jk")
   (setq mac-command-modifier 'control)
   (setq mac-control-modifier 'meta)
   )
