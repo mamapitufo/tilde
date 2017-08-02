e0bb28696277a754b48962225e537da91db81a73
Alberto Brealey-Guzman <alberto@feltfragments.com>
2017-08-02 17:37:08 +0100

    Inital fish configuration

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,7 @@
+set fish_greeting ""
+set -x LANG en_GB.UTF-8
+
+set -u EDITOR nvim
+set -u VISUAL nvim
+set -u BROWSER chromium
+
---
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -0,0 +1,16 @@
+set fish_color_cwd blue
+
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+
+function fish_prompt --description "Write out the prompt"
+	set_color $fish_color_cwd
+	printf "%s" (prompt_pwd)
+	set_color normal
+
+	printf "%s λ " (__fish_git_prompt)
+end
+
