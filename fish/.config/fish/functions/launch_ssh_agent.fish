2da18a9adef8e88716cf3197630d2d8eaf3f6d8d
Alberto Brealey-Guzman <alberto@feltfragments.com>
2017-08-07 13:04:08 +0100

    Fix `launch_ssh_agent` function
    It was completely broken, launching multiple agents...

--- fish/.config/fish/functions/launch_ssh_agent.fish
+++ fish/.config/fish/functions/launch_ssh_agent.fish
@@ -1,15 +1,23 @@
-setenv SSH_AGENT_ENV $HOME/.ssh/ssh-agent-env
-
 function launch_ssh_agent --description "Launch `ssh-agent` if it is not running, source the environment variables otherwise"
-	if set -q SSH_AGENT_PID
-		set -l pid (pgrep -u $USER ssh-agent)
-		if test $pid -ne $SSH_AGENT_PID
-			log_error '$SSH_AGENT_PID information is out of date!'
-		end
-	else
-		ssh-agent -c | sed "s/^echo/#echo/" > $SSH_AGENT_ENV
+	set -l SSH_AGENT_ENV $HOME/.ssh/ssh-agent-env
+	set -l pid (pgrep -u $USER ssh-agent)
+	set -l agent_count (count $pid)
+
+	switch $agent_count
+		case 0
+			ssh-agent -c | sed "s/^echo/#echo/" > $SSH_AGENT_ENV
+		case 1
+			if set -q SSH_AGENT_PID[0]; and test $pid -ne $SSH_AGENT_PID
+				log_error "$SSH_AGENT_PID information is out of date! Restarting..."
+				kill $pid
+				launch_ssh_agent
+			end
+		case "*"
+			log_error "There is more than one ssh-agent running! Restarting..."
+			for ag in $pid; kill $ag; end
+			launch_ssh_agent
 	end
 
-	. $SSH_AGENT_ENV
+	source $SSH_AGENT_ENV
 end
 
