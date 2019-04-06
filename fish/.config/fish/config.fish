ed9db7b7b936e5610ed4aaaa561a78ee766f26cb
Alberto Brealey-Guzman <alberto@feltfragments.com>
2019-04-06 14:20:55 +0100

    Make KiTTY the default terminal

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,14 +6,13 @@ set --universal fish_user_paths ~/bin ~/bin/gem ~/.npm-packages/bin ~/.cargo/bin
 set -ge LS_COLORS
 
 set -x LANG en_GB.UTF-8
-set -x TERMINAL alacritty
+set -x TERMINAL kitty
 
 set -gx EDITOR nvim
 set -gx VISUAL nvim
 set -gx BROWSER firefox
 
-# Infinality mode in freetype (see
-# http://blog.brunomiguel.net/geekices/how-to-get-good-font-rendering-in-void-linux)
+# Infinality mode in freetype (see http://blog.brunomiguel.net/geekices/how-to-get-good-font-rendering-in-void-linux)
 set -u FREETYPE_PROPERTIES "truetype:interpreter-version=38"
 
 launch_gnome_keyring
