1aa0f86537f2a11a25f380d414069a881750b60f
Alberto Brealey-Guzman <alberto@feltfragments.com>
2019-04-14 20:24:05 +0100

    Update fish prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,4 +1,5 @@
 set fish_color_cwd blue
+set fish_color_time white
 
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate 'yes'
@@ -7,10 +8,13 @@ set __fish_git_prompt_showupstream 'yes'
 set __fish_git_prompt_color_branch yellow
 
 function fish_prompt --description "Write out the prompt"
+	printf "┌ "
+	set_color $fish_color_time
+	printf "%s" (date "+%d/%m %H:%M:%S ")
 	set_color $fish_color_cwd
-	printf "%s" (prompt_pwd)
-	set_color normal
+	printf "%s" (pwd)
 
-	printf "%s λ " (__fish_git_prompt)
+	set_color normal
+	printf "\n└%s λ " (__fish_git_prompt)
 end
 
--- fish/.config/fish/functions/fish_right_prompt.fish
+++
@@ -1,5 +0,0 @@
-function fish_right_prompt --description "Write out the right-hand side of the prompt"
-	set_color cyan --bold
-	date "+%Y-%m-%d %H:%M:%S"
-end
-
