74015f83fc624dc55a48bdd853a96ff693d383e7
Alberto Brealey-Guzman <alberto@feltfragments.com>
2019-04-18 16:20:03 +0100

    Use nicer `pwd` display for fish prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,6 +1,8 @@
 set fish_color_cwd blue
 set fish_color_time white
 
+set fish_prompt_pwd_dir_length 0
+
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate 'yes'
 set __fish_git_prompt_showuntrackedfiles 'yes'
@@ -12,7 +14,7 @@ function fish_prompt --description "Write out the prompt"
 	set_color $fish_color_time
 	printf "%s" (date "+%d/%m %H:%M:%S ")
 	set_color $fish_color_cwd
-	printf "%s" (pwd)
+	printf "%s" (prompt_pwd)
 
 	set_color normal
 	printf "\n└%s λ " (__fish_git_prompt)
