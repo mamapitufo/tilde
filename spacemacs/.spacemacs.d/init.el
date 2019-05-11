090acfdf9eab1c24f06dd3e32baf124ca822c9c6
Alberto Brealey-Guzman <alberto@feltfragments.com>
2019-05-11 18:03:01 +0100

    spacemacs: Use light theme by default

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -200,8 +200,8 @@ It should only modify the values of Spacemacs settings."
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
 
-   dotspacemacs-themes '(phoenix-dark-mono
-                         espresso)
+   dotspacemacs-themes '(espresso
+                         phoenix-dark-mono)
                          ;; oldlace
                          ;; ritchie
                          ;; minimal-light
