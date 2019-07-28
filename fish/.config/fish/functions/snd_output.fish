94e23d7059e24436812da70ac9633347bf3e97c6
Alberto Brealey-Guzman <alberto@feltfragments.com>
2019-07-28 11:21:19 +0100

    fish: mute and volume now handled by pulse applet

--- fish/.config/fish/functions/snd_output.fish
+++
@@ -1,9 +0,0 @@
-function snd_output --description "Prints out the current output (headphones, speaker, etc)"
-	set headphone_count (amixer contents| grep -A2 "'Headphone Jack'"| grep -c ": values=on")
-	if test $headphone_count -gt 0
-		echo "Headphone"
-	else
-		echo "Speaker"
-	end
-end
-
