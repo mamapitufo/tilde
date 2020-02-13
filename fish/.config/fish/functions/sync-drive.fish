df1898e8e79a816c99a74f0bc007e1608d9acead
Alberto Brealey-Guzman <alberto@feltfragments.com>
2020-02-13 18:47:54 +0000

    fish: Fix broken gDrive sync function

--- fish/.config/fish/functions/sync-drive.fish
+++ fish/.config/fish/functions/sync-drive.fish
@@ -1,4 +1,4 @@
 function sync-drive --description "Syncs gDrive"
-  set -l changed find . -cnewer last-sync -type d
-  drive push (changed); and touch last-sync
+  set --local changed find . -cnewer last-sync -type d
+  drive push ($changed); and touch last-sync
 end
