0a6a1a30e215b4cc66307e8956e3d865094ccc34
Alberto Brealey-Guzman <alberto@feltfragments.com>
2020-05-31 18:49:00 +0100

    fish: Skip symlinks in `dir_count` command

--- fish/.config/fish/functions/dir_count.fish
+++ fish/.config/fish/functions/dir_count.fish
@@ -1,5 +1,5 @@
 function dir_count --description "Prints the number of files in each directory in the current directory"
-	for dir in (ls -d */)
-		echo $dir (count $dir/*)
+	for dir in (find . -maxdepth 1 -mindepth 1 -type d)
+		echo $dir: (count $dir/*)
 	end
 end
