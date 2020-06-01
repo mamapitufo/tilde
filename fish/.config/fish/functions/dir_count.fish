a5e4182b874169d65e91a55b4a55ab14d6c79e5f
Alberto Brealey-Guzman <alberto@feltfragments.com>
2020-06-01 11:22:32 +0100

    fish: sort `dir_count` results

--- fish/.config/fish/functions/dir_count.fish
+++ fish/.config/fish/functions/dir_count.fish
@@ -1,5 +1,5 @@
 function dir_count --description "Prints the number of files in each directory in the current directory"
-	for dir in (find . -maxdepth 1 -mindepth 1 -type d)
+	for dir in (find . -maxdepth 1 -mindepth 1 -type d| sort)
 		echo $dir: (count $dir/*)
 	end
 end
