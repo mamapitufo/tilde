19f142341be10a3beb9af3f8e47199f64a8099b8
Alberto Brealey-Guzman <alberto@feltfragments.com>
2021-06-09 22:31:03 +0100

    nvim: Close telescope window with <esc>

--- neovim/.config/nvim/plugins/telescope.nvim.lua
+++ neovim/.config/nvim/plugins/telescope.nvim.lua
@@ -1,13 +1,19 @@
+local actions = require('telescope.actions')
 require('telescope').setup{
-	defaults = {
-		prompt_position = "top",
-	},
-	extensions = {
-		fzy_native = {
-			override_generic_sorter = false,
-			override_file_sorter = true,
-		}
-	}
+  defaults = {
+    prompt_position = "top",
+    mappings = {
+      i = {
+        ['<esc>'] = actions.close
+      }
+    }
+  },
+  extensions = {
+    fzy_native = {
+      override_generic_sorter = false,
+      override_file_sorter = true,
+    }
+  }
 }
 
 require('telescope').load_extension('fzy_native')
