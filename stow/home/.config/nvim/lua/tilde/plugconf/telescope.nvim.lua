ff9b4cc25f9f09cc58cadff1ad6b2bebc0ed43dc
Alberto Brealey-Guzman <alberto@feltfragments.com>
2022-05-20 16:54:58 +0100

    nvim: Add a few more Telescope actions
    Updated key mapping API and securely loaded which-key.

--- stow/home/.config/nvim/lua/tilde/plugconf/telescope.nvim.lua
+++ stow/home/.config/nvim/lua/tilde/plugconf/telescope.nvim.lua
@@ -1,5 +1,3 @@
-local actions = require('telescope.actions')
-
 require('telescope').setup{
   defaults = {
     file_ignore_patterns = {'node_modules'},
@@ -23,26 +21,42 @@ require('telescope').setup{
   },
 }
 
-local map = vim.api.nvim_set_keymap
-local map_opts = { silent = true, noremap = true }
-map('n', '<leader>bb', ':Telescope buffers<cr>', map_opts)
-map('n', '<leader>fc', ':Telescope find_files hidden=true cwd=$HOME/Sandbox/tilde prompt_prefix=\\ ~\\  prompt_title=Sandbox/tilde<cr>', map_opts)
-map('n', '<leader>ff', ':lua require\'tilde.telescope-extras\'.git_files_fallback()<cr>', map_opts)
-map('n', '<leader>*', ':Telescope grep_string<cr>', map_opts)
-map('n', '<leader>s/', ':Telescope search_history<cr>', map_opts)
-map('n', '<leader>s:', ':Telescope command_history<cr>', map_opts)
-map('n', '<leader>sf', ':Telescope live_grep<cr>', map_opts)
-map('n', '<leader>sh', ':Telescope help_tags<cr>', map_opts)
+local function map(mode, l, r, opts)
+  opts = opts or {}
+  vim.keymap.set(mode, l, r, opts)
+end
 
-require('which-key').register({
+map('n', '<leader>bb', ':Telescope buffers<cr>')
+map('n', '<leader>fc', ':Telescope find_files hidden=true cwd=$HOME/Sandbox/tilde prompt_prefix=\\ ~\\  prompt_title=Sandbox/tilde<cr>')
+map('n', '<leader>ff', ':lua require\'tilde.telescope-extras\'.git_files_fallback()<cr>')
+map('n', '<leader>fr', ':Telescope oldfiles<cr>')
+map('n', '<leader>*', ':Telescope grep_string<cr>')
+map('n', '<leader>s/', ':Telescope search_history<cr>')
+map('n', '<leader>s:', ':Telescope command_history<cr>')
+map('n', '<leader>sb', ':Telescope current_buffer_fuzzy_find<cr>')
+map('n', '<leader>sf', ':Telescope live_grep<cr>')
+map('n', '<leader>sh', ':Telescope help_tags<cr>')
+map('n', '<leader>gl', ':Telescope git_commits<cr>')
+map('n', '<leader>glb', ':Telescope git_bcommits<cr>')
+
+local status_ok, which_key = pcall(require, 'which-key')
+if not status_ok then
+  return
+end
+
+which_key.register({
   ['<leader>'] = {
     bb = 'Find buffer',
     fc = 'Find config file',
     ff = 'Find file',
+    fr = 'Find recently opened file',
     ['*'] = 'Find string under cursor',
     ['s/'] = 'Find in search history',
     ['s:'] = 'Find in command history',
+    sb = 'Fuzzy find in current buffer',
     sf = 'Grep in project',
     sh = 'Find help',
+    gl = 'Git log',
+    glb = 'Git log for buffer file',
   },
 })
