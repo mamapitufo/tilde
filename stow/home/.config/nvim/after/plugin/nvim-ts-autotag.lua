462b80452f9f02ed1f994b7ed0b815c1aabeb73e
Alberto Brealey-Guzman <alberto@feltfragments.com>
2022-11-28 12:07:46 +0000

    nvim: Tweak treesitter and autopairs config
    nvim: Simplify auto-tag configuration

--- stow/home/.config/nvim/after/plugin/nvim-ts-autotag.lua
+++ stow/home/.config/nvim/after/plugin/nvim-ts-autotag.lua
@@ -2,8 +2,4 @@ if not require("tilde.utils").assert_plug("nvim-ts-autotag") then
 	return
 end
 
-require("nvim-treesitter.configs").setup({
-	autotag = {
-		enable = true,
-	},
-})
+require("nvim-ts-autotag").setup({})
--- stow/home/.config/nvim/after/plugin/treesitter.lua
+++ stow/home/.config/nvim/after/plugin/treesitter.lua
@@ -16,6 +16,7 @@ require("nvim-treesitter.configs").setup({
 		"scss",
 		"tsx",
 		"typescript",
+		"toml",
 		"vim",
 		"vue",
 		"yaml",
