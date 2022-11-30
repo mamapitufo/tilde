bb34e736b21c7437c830f503581aed3aae776710
Alberto Brealey-Guzman <alberto@feltfragments.com>
2022-11-30 10:02:54 +0000

    nvim: Simplify plugin config names

--- stow/home/.config/nvim/after/plugin/file-tree.lua
+++ stow/home/.config/nvim/after/plugin/file-tree.lua
@@ -5,3 +5,4 @@ end
 require("nvim-tree").setup()
 
 vim.keymap.set("n", "<leader>tt", ":NvimTreeToggle<cr>", { desc = "Toggle file browser" })
+vim.keymap.set("n", "<leader>ft", ":NvimTreeFindFile<cr>", { desc = "Select in file browser" })
--- stow/home/.config/nvim/after/plugin/luasnip.lua
+++ stow/home/.config/nvim/after/plugin/luasnip.lua
@@ -13,18 +13,13 @@ luasnip.config.set_config({
 require("luasnip.loaders.from_lua").load()
 
 -- keyboard mappings
-vim.keymap.set({ "i", "s" }, "<c-enter>", function()
+vim.keymap.set({ "i", "s" }, "<c-l>", function()
 	if luasnip.expand_or_jumpable() then
 		luasnip.expand_or_jump()
 	end
 end, { silent = true })
-vim.keymap.set({ "i", "s" }, "<c-j>", function()
+vim.keymap.set({ "i", "s" }, "<c-h>", function()
 	if luasnip.jumpable(-1) then
 		luasnip.jump(-1)
-	end
-end, { silent = true })
-vim.keymap.set("i", "<c-l>", function()
-	if luasnip.choice.active() then
-		luasnip.change_choice(1)
 	end
 end, { silent = true })
