dc67b0936f4bc6c1f7e1d6ef9e8c9901faa01947
Alberto Brealey-Guzman <alberto@feltfragments.com>
2022-12-02 13:55:18 +0000

    nvim: Cleaner "untracked file" gutter signs

--- stow/home/.config/nvim/after/plugin/gitsigns.lua
+++ stow/home/.config/nvim/after/plugin/gitsigns.lua
@@ -57,6 +57,7 @@ end
 require("gitsigns").setup({
 	signs = {
 		add = { hl = "GitSignsAdd", text = "+", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+		untracked = { hl = "GitSignsAdd", text = "∶", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
 		change = { hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
 		delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
 		topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
