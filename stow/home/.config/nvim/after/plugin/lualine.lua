0039840af648c3d593b51ea6a0a906f7b816704f
Alberto Brealey-Guzman <alberto@feltfragments.com>
2022-12-12 18:46:25 +0000

    nvim: Remove unnecessary config for lualine
    This was causing warnings, and it's almost the same as the default.

--- stow/home/.config/nvim/after/plugin/lualine.lua
+++ stow/home/.config/nvim/after/plugin/lualine.lua
@@ -51,14 +51,6 @@ require("lualine").setup({
       },
       "progress",
     },
-    inactive_sections = {
-      lualine_a = {},
-      lualine_b = {},
-      lualine_c = { "filename", path = 1 },
-      lualine_x = { "location" },
-      luanine_y = {},
-      lualine_z = {},
-    },
   },
   extensions = { "quickfix", "fugitive" },
 })
