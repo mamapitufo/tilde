af41b0ae2870200c90a5a378ded70502533e1d38
Alberto Brealey-Guzman <alberto@feltfragments.com>
2023-02-13 16:06:26 +0000

    fish: Add Anaconda path

--- stow/home/.config/fish/config.fish
+++ stow/home/.config/fish/config.fish
@@ -1,6 +1,6 @@
 set fish_greeting ""
 
-set --universal fish_user_paths ~/.local/bin ~/bin ~/bin/gem ~/.npm-packages/bin ~/.cargo/bin ~/Library/charles/bin
+set --universal fish_user_paths ~/.local/bin ~/bin ~/bin/gem ~/.npm-packages/bin ~/.cargo/bin ~/Library/charles/bin ~/Library/anaconda3/bin
 
 #-- tide prompt config
 set --global tide_left_prompt_frame_enabled true
@@ -63,3 +63,9 @@ end
 if test -d ~/.linuxbrew
   eval (~/.linuxbrew/bin/brew shellenv)
 end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /home/mamapitufo/Library/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
