14e43c5564ff5bb44612f14df81b002632de2778
Alberto Brealey-Guzman <alberto@feltfragments.com>
2023-04-06 17:11:06 +0100

    nvim: Add autocomplete

--- stow/home/.config/nvim/init.lua
+++ stow/home/.config/nvim/init.lua
@@ -90,6 +90,17 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'hrsh7th/nvim-cmp',                     -- Autocompletion
+    dependencies = {
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-nvim-lsp',
+      'dcampos/nvim-snippy',
+      'dcampos/cmp-snippy',
+    },
+  },
+
   -- -- Git related plugins
   -- 'tpope/vim-fugitive',
   -- 'tpope/vim-rhubarb',
@@ -110,11 +121,6 @@ require('lazy').setup({
   --     -- Additional lua configuration, makes nvim stuff amazing!
   --     'folke/neodev.nvim',
   --   },
-  -- },
-  --
-  -- { -- Autocompletion
-  --   'hrsh7th/nvim-cmp',
-  --   dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
   -- },
   --
   -- { -- Adds git releated signs to the gutter, as well as utilities for managing changes
@@ -419,6 +425,51 @@ kmap('n', '<leader>sh', tbuiltin.help_tags, { desc = 'Find help' })
 kmap('n', '<leader>gl', tbuiltin.git_commits, { desc = 'Git log' })
 kmap('n', '<leader>glb', tbuiltin.git_bcommits, { desc = 'Git log for buffer' })
 
+-- [[ 'hrsh7th/nvim-cmp' setup ]]
+local cmp = require 'cmp'
+
+cmp.setup {
+  completion = {
+    autocomplete = false
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'conjure' },
+    { name = 'snippy' },
+    { name = 'path' },
+    { name = 'buffer' },
+  }),
+  formatting = {
+    format = function(entry, vim_item)
+      vim_item.menu = ({
+        buffer = '[buff]',
+        conjure = '[conj]',
+        snippy = '[snip]',
+        nvim_lsp = '[lsp]',
+        path = '[path]',
+      })[entry.source.name]
+      return vim_item
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+    ['<C-space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<cr>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+  }),
+  snippet = {
+    expand = function(args)
+      require('snippy').expand_snippet(args.body)
+    end,
+  },
+}
+
 -- -- [[ Configure Treesitter ]]
 -- -- See `:help nvim-treesitter`
 -- require('nvim-treesitter.configs').setup {
@@ -577,49 +628,5 @@ kmap('n', '<leader>glb', tbuiltin.git_bcommits, { desc = 'Git log for buffer' })
 --   end,
 -- }
 --
--- -- nvim-cmp setup
--- local cmp = require 'cmp'
--- local luasnip = require 'luasnip'
---
--- luasnip.config.setup {}
---
--- cmp.setup {
---   snippet = {
---     expand = function(args)
---       luasnip.lsp_expand(args.body)
---     end,
---   },
---   mapping = cmp.mapping.preset.insert {
---     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
---     ['<C-f>'] = cmp.mapping.scroll_docs(4),
---     ['<C-Space>'] = cmp.mapping.complete {},
---     ['<CR>'] = cmp.mapping.confirm {
---       behavior = cmp.ConfirmBehavior.Replace,
---       select = true,
---     },
---     ['<Tab>'] = cmp.mapping(function(fallback)
---       if cmp.visible() then
---         cmp.select_next_item()
---       elseif luasnip.expand_or_jumpable() then
---         luasnip.expand_or_jump()
---       else
---         fallback()
---       end
---     end, { 'i', 's' }),
---     ['<S-Tab>'] = cmp.mapping(function(fallback)
---       if cmp.visible() then
---         cmp.select_prev_item()
---       elseif luasnip.jumpable(-1) then
---         luasnip.jump(-1)
---       else
---         fallback()
---       end
---     end, { 'i', 's' }),
---   },
---   sources = {
---     { name = 'nvim_lsp' },
---     { name = 'luasnip' },
---   },
--- }
 --
 -- vim: ts=2 sts=2 sw=2 et
