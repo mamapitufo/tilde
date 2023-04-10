147cdb782580da5bd41a962047a236ef15d3a002
Alberto Brealey-Guzman <alberto@feltfragments.com>
2023-04-10 15:11:25 +0100

    nvim: Lazy load treesitter

--- stow/home/.config/nvim/lua/tilde/plugins/editor.lua
+++ stow/home/.config/nvim/lua/tilde/plugins/editor.lua
@@ -56,4 +56,100 @@ return {
       }
     end,
   },
+  {
+    'nvim-treesitter/nvim-treesitter',      -- Highlight, edit, and navigate code
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    event = { "BufReadPost", "BufNewFile" },
+    keys = {
+      { '<c-space>', desc = 'Increment selection' },
+      { '<bs>', desc = 'Decrement selection', mode = 'x' },
+    },
+    opts = {
+      ensure_installed = {
+        'clojure',
+        'css',
+        'fish',
+        'graphql',
+--        'help',
+        'html',
+        'javascript',
+        'json',
+        'jsonc',
+        'json5',
+        'lua',
+        'rasi',
+        'regex',
+        'scss',
+        'tsx',
+        'typescript',
+        'toml',
+        'vim',
+        'yaml',
+      },
+      auto_install = false,
+
+      highlight = { enable = true },
+      indent = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<c-space>',
+          node_incremental = '<c-space>',
+          scope_incremental = '<c-s>',
+          node_decremental = '<bs>',
+        },
+      },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ['aa'] = '@parameter.outer',
+            ['ia'] = '@parameter.inner',
+            ['af'] = '@function.outer',
+            ['if'] = '@function.inner',
+            ['ac'] = '@class.outer',
+            ['ic'] = '@class.inner',
+          },
+        },
+        move = {
+          enable = true,
+          set_jumps = true, -- whether to set jumps in the jumplist
+          goto_next_start = {
+            [']m'] = '@function.outer',
+            [']]'] = '@class.outer',
+          },
+          goto_next_end = {
+            [']M'] = '@function.outer',
+            [']['] = '@class.outer',
+          },
+          goto_previous_start = {
+            ['[m'] = '@function.outer',
+            ['[['] = '@class.outer',
+          },
+          goto_previous_end = {
+            ['[M'] = '@function.outer',
+            ['[]'] = '@class.outer',
+          },
+        },
+        swap = {
+          enable = true,
+          swap_next = {
+            ['<leader>a'] = '@parameter.inner',
+          },
+          swap_previous = {
+            ['<leader>A'] = '@parameter.inner',
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      require('nvim-treesitter.configs').setup(opts)
+
+      pcall(require('nvim-treesitter.install').update { with_sync = true })
+    end,
+  },
 }
--- stow/home/.config/nvim/lua/tilde/plugins/syntax.lua
+++ stow/home/.config/nvim/lua/tilde/plugins/syntax.lua
@@ -7,16 +7,6 @@ return {
       vim.g.asciidoctor_fenced_languages = { 'bash', 'clojure', 'css', 'html', 'javascript', 'json', 'rust', 'sh', }
     end,
   },
-
-  {
-    'nvim-treesitter/nvim-treesitter',      -- Highlight, edit, and navigate code
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
-    config = function()
-      pcall(require('nvim-treesitter.install').update { with_sync = true })
-    end,
-  },
   {
     'norcalli/nvim-colorizer.lua',          -- Preview hex colours in background
     event = 'VeryLazy',
