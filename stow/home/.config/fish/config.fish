7943fb3cb87282e1814557da59dfcd09cf65147a
Alberto Brealey-Guzman <alberto@feltfragments.com>
2023-04-21 18:51:49 +0100

    Remove homebrew setup
    All tools installed via homebrew are now managed by mason or void Linux

--- stow/home/.config/fish/config.fish
+++ stow/home/.config/fish/config.fish
@@ -60,13 +60,7 @@ if status is-interactive; and test -f $theme_path
   source $theme_path
 end
 
-# Homebrew
-if test -d ~/.linuxbrew
-  eval (~/.linuxbrew/bin/brew shellenv)
-end
-
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 eval /home/mamapitufo/Library/anaconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
-
