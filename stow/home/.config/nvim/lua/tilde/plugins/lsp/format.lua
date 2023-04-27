928a4afc3c6bd0255f24a3553963b684f5a30fd9
Alberto Brealey-Guzman <alberto@feltfragments.com>
2023-04-27 20:33:23 +0100

    nvim: Lua formatting

--- stow/home/.config/nvim/lua/tilde/plugins/lsp/format.lua
+++ stow/home/.config/nvim/lua/tilde/plugins/lsp/format.lua
@@ -2,21 +2,23 @@ local M = {}
 
 -- this receives the same opts as `vim.lsp.buf.format`
 function M.format(opts)
-  vim.lsp.buf.format({
+  vim.lsp.buf.format {
     bufnr = opts.bufnr,
     filter = function(client)
       return client.name ~= 'tsserver'
     end,
-  })
+  }
 end
 
 function M.on_attach(client, bufnr)
   -- TODO: there should be a toggle for auto-format on write
-  if client.supports_method('textDocument/formatting') then
+  if client.supports_method 'textDocument/formatting' then
     vim.api.nvim_create_autocmd('BufWritePre', {
-      callback = function() M.format({ bufnr = bufnr }) end,
+      callback = function()
+        M.format { bufnr = bufnr }
+      end,
       buffer = bufnr,
-      group = vim.api.nvim_create_augroup('LSPFormatOnSave' .. bufnr, {})
+      group = vim.api.nvim_create_augroup('LSPFormatOnSave' .. bufnr, {}),
     })
   end
 end
