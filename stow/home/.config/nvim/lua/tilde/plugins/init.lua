8d5bee18018460d40a3ef1fdb30bd4ed1b2a411b
Alberto Brealey-Guzman <alberto@feltfragments.com>
2023-04-30 09:28:50 +0100

    nvim: Add directory tree explorer

--- stow/home/.config/nvim/lua/tilde/plugins/init.lua
+++ stow/home/.config/nvim/lua/tilde/plugins/init.lua
@@ -39,7 +39,7 @@ return {
       select = {
         backend = { 'builtin', 'telescope' },
       },
-    }
+    },
   },
   -- go fwd/back with bracket pairs
   {
@@ -47,5 +47,54 @@ return {
     config = function(_, opts)
       require('mini.bracketed').setup(opts)
     end,
+  },
+  -- file tree browser
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v2.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+    },
+    cmd = 'Neotree',
+    keys = {
+      {
+        '<leader>te',
+        function()
+          require('neo-tree.command').execute { toggle = true }
+        end,
+        desc = 'File explorer',
+      },
+      {
+        '<leader>fe',
+        ':Neotree reveal_file=%<cr>',
+        desc = 'Reveal in file explorer',
+      },
+    },
+    init = function()
+      vim.g.neo_tree_remove_legacy_commands = 1
+    end,
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+        },
+      },
+      window = {
+        position = 'right',
+        mappings = {
+          ['<space>'] = 'none',
+        },
+      },
+      default_component_configs = {
+        indent = {
+          with_markers = false,
+          with_expanders = true,
+          expander_collapsed = '',
+          expander_expanded = '',
+          expander_highlight = 'NeoTreeExpander',
+        },
+      },
+    },
   },
 }
