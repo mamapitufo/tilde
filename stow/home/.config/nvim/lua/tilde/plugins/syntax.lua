6df2bd5ac136a5c2d49949908451c5b1ecc1b71a
Alberto Brealey-Guzman <alberto@feltfragments.com>
2023-04-30 18:25:13 +0100

    nvim: lazy-load syntax plugins

--- stow/home/.config/nvim/lua/tilde/plugins/syntax.lua
+++ stow/home/.config/nvim/lua/tilde/plugins/syntax.lua
@@ -1,10 +1,11 @@
 return {
-  'blankname/vim-fish',
-  'kmonad/kmonad-vim',
+  { 'blankname/vim-fish', ft = 'fish' },
+  { 'kmonad/kmonad-vim', ft = 'kbd' },
   {
     'habamax/vim-asciidoctor',
+    ft = 'asciidoctor',
     config = function()
-      vim.g.asciidoctor_fenced_languages = { 'bash', 'clojure', 'css', 'html', 'javascript', 'json', 'rust', 'sh', }
+      vim.g.asciidoctor_fenced_languages = { 'bash', 'clojure', 'css', 'html', 'javascript', 'json', 'rust', 'sh' }
     end,
   },
   -- Preview hex colours in background
