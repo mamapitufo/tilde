cc261a0575920169f071cc0cebb28f806b76dcf9
Alberto Brealey-Guzman <alberto@feltfragments.com>
2024-02-11 21:29:43 +0000

    fish: Add aliases for ProtonVPN

--- stow/home/.config/fish/config.fish
+++ stow/home/.config/fish/config.fish
@@ -56,6 +56,9 @@ alias disown-all "disown (jobs -g)"
 
 alias jstree "tree -I node_modules"
 
+alias vpn-on "doas /usr/bin/protonvpn connect --cc"
+alias vpn-off "doas /usr/bin/protonvpn disconnect"
+
 if status is-interactive; and test $TERM = "xterm-kitty"
   alias tmux "tmux -2"
 end
