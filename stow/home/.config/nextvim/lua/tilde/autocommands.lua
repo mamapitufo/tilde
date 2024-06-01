97059211fb2f9f0b4502d048a19ebea83270aa3c
Alberto Brealey-Guzman <alberto@feltfragments.com>
2024-06-01 17:04:06 +0100

    nvim: Reorg `init.lua`
    Simple file that calls a few modules; moved the plugin manager and
    configuration to its own module.

---
+++ stow/home/.config/nextvim/lua/tilde/autocommands.lua
@@ -0,0 +1,1 @@
+-- [[ Basic Autocommands ]]
---
+++ stow/home/.config/nextvim/lua/tilde/plugin-manager.lua
@@ -0,0 +1,15 @@
+-- [[ `lazy.nvim` plugin manager (https://github.com/folke/lazy.nvim) ]]
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup('tilde.plugins', {
+  install = {
+    colorscheme = { 'zenbones', 'habamax' },
+  },
+})
+
+-- vim: ts=2 sts=2 sw=2 et
