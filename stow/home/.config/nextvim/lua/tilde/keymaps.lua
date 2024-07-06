5e1aa4b843db68f9820d0aca8fdc79401953087f
Alberto Brealey-Guzman <alberto@feltfragments.com>
2024-07-06 20:35:03 +0100

    nvim: Add a few more key mappings

--- stow/home/.config/nextvim/lua/tilde/keymaps.lua
+++ stow/home/.config/nextvim/lua/tilde/keymaps.lua
@@ -27,14 +27,38 @@ end, { desc = 'Next error' })
 vim.keymap.set('n', '<leader>dl', function()
   vim.diagnostic.setloclist { open = true }
 end, { desc = 'Send diagnostics to Location list' })
-vim.keymap.set('n', '<leader>ds', vim.diagnostic.open_float, { desc = 'Show diagnostings' })
+vim.keymap.set('n', '<leader>ds', vim.diagnostic.open_float, { desc = 'Show errors' })
 
 vim.keymap.set('t', '<esc><esc>', '<c-\\><c-n>', { desc = 'Exit terminal mode' })
+
+-- file
+vim.keymap.set('n', '<leader>fs', '<cmd>update<cr>', { desc = 'Save file' })
 
 -- split navigation
 vim.keymap.set('n', '<leader>wh', '<c-w>h', { desc = 'Move focus to the window on the left' })
 vim.keymap.set('n', '<leader>wl', '<c-w>l', { desc = 'Move focus to the window on the right' })
 vim.keymap.set('n', '<leader>wj', '<c-w>j', { desc = 'Move focus to the window below' })
 vim.keymap.set('n', '<leader>wk', '<c-w>k', { desc = 'Move focus to the window above' })
+
+-- quit
+vim.keymap.set('n', '<leader>qQ', '<cmd>qa!<cr>', { desc = 'Force quit' })
+vim.keymap.set('n', '<leader>qq', '<cmd>qa<cr>', { desc = 'Close all windows and quit' })
+
+-- window
+vim.keymap.set('n', '<leader>wk', '<c-w>k', { desc = 'Focus above' })
+vim.keymap.set('n', '<leader>wj', '<c-w>j', { desc = 'Focus below' })
+vim.keymap.set('n', '<leader>wh', '<c-w>h', { desc = 'Focus left' })
+vim.keymap.set('n', '<leader>wl', '<c-w>l', { desc = 'Focus right' })
+vim.keymap.set('n', '<leader>wq', '<c-w>q', { desc = 'Close' })
+vim.keymap.set('n', '<leader>ws', '<c-w>s', { desc = 'Split horizontally' })
+vim.keymap.set('n', '<leader>wv', '<c-w>v', { desc = 'Split vertically' })
+vim.keymap.set('n', '<leader>ww', '<c-w>w', { desc = 'Switch windows' })
+vim.keymap.set('n', '<leader>wx', '<c-w>x', { desc = 'Swap with next window' })
+vim.keymap.set('n', '<leader>w=', '<c-w>=', { desc = 'Make all windows the same size' })
+vim.keymap.set('n', '<leader>wK', ':resize -5<cr>', { desc = 'Decrease height' })
+vim.keymap.set('n', '<leader>wJ', ':resize +5<cr>', { desc = 'Increase height' })
+vim.keymap.set('n', '<leader>wH', '<c-w>5<', { desc = 'Decrease width' })
+vim.keymap.set('n', '<leader>wL', '<c-w>5>', { desc = 'Increase width' })
+vim.keymap.set('n', '<leader>w<bar>', '<c-w><bar>', { desc = 'Maximize width' })
 
 -- vim: ts=2 sts=2 sw=2 et
